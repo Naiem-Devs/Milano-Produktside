df460ac8037fa4634effb1a6bf3a3ca5205613ed
Naiem-Devs <dnsnaiem@gmail.com>
2025-02-09 22:30:54 +0600

    one

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,31 @@
     $(this).toggleClass('active');   
     $('.mobile-menu').toggleClass('siteBar');   
   }); 
+  
+
+ //  
+ $('.sofaBtn').click( function (event){ 
+    event.preventDefault()
+    $(".p__two").addClass('show');  
+
+    $(".p__one").removeClass('show');        
+    $(".p__three").removeClass('show');      
+  });
+
+  $('.textureBtn').click( function (event){ 
+    event.preventDefault()
+    $(".p__three").addClass('show');  
+
+    $(".p__one").removeClass('show');        
+    $(".p__two").removeClass('show');      
+  });
+
+ $('.tilbage__link a').click( function (event){ 
+    event.preventDefault() 
+	$(".p__one").addClass('show');
+    $(".p__two").removeClass('show');        
+    $(".p__three").removeClass('show');        
+  }); 
 
 
 
